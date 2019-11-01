9150f2eb93bf181cce11e769afbab9bb910e3756
Dmitry Pavlov <pvlv@mail.ru>
2019-11-01 12:48:58 +0500

    Initial Commit

---
+++ TMDBapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TMDBapp
+//
+//  Created by Dmitry Pavlov on 01/11/2019.
+//  Copyright © 2019 Dmitry Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
